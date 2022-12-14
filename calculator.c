b1f4b0a7b1b886548d1c8e9c4815074629ef6ddc
unit74 <u__nit@naver.com>
2022-12-14 22:30:48 +0900

    201930280 이동규

--- calculator.c
+++ calculator.c
@@ -5,7 +5,7 @@
 
 int main(void) {
 	char input[BUFFERSIZE];
-	
+	printf("This printf is for assignment3\n");
 	printf("Enter your expression: ");
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
