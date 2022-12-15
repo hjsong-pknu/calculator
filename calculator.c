edf7889b09642414502946a8413aad44ec183111
onegqueen <rkdwldnjs30@naver.com>
2022-12-15 13:47:31 +0900

    202011288 강지원 calculator.c 파일에 출력문을 추가함

--- calculator.c
+++ calculator.c
@@ -6,6 +6,7 @@
 int main(void) {
 	char input[BUFFERSIZE];
 	
+	printf("Hello, This is Jiwon's caculator. Have a good day!");
 	printf("Enter your expression: ");
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
