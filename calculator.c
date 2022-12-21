928a7ae259cf25a1bbf6f1e3ff828753426f16ad
choieunhee <cheh020511@naver.com>
2022-12-21 18:44:42 +0900

    Change calculator.c

--- calculator.c
+++ calculator.c
@@ -5,12 +5,12 @@
 
 int main(void) {
 	char input[BUFFERSIZE];
-	
+        printf("This is mycalculator");
 	printf("Enter your expression: ");
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
 	printf("Result is: %.2f\n", evaluate(input));
-	printf("This is mycalculator");
+
 	return EXIT_SUCCESS;
 
 }
