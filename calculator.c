6859da84c5c432a4059967636737b14c8c385b60
김민재 <57130672+minJJJ@users.noreply.github.com>
2022-12-22 22:55:36 +0900

    201612688 김민재 calculator.c에 변수 a 추가

--- calculator.c
+++ calculator.c
@@ -5,14 +5,12 @@
 
 
 int main(void) {
-
 	char input[BUFFERSIZE];
+	int a;
 	
 	printf("Enter your expression: ");
 	fflush(stdout);
-
 	fgets(input, BUFFERSIZE, stdin);
-
 	printf("Result is: %.2f\n", evaluate(input));
 	return EXIT_SUCCESS;
 }
