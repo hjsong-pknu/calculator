fd00cea723bf6ddd11db021e479f8077988a9b70
Myeong JIn-Kim <k4aaud@gmail.com>
2022-12-23 19:48:26 +0900

    Myeong-Jin-Kim-201830164

--- calculator.c
+++ calculator.c
@@ -5,7 +5,7 @@
 
 int main(void) {
 	char input[BUFFERSIZE];
-	
+	printf("Proffesor Mr HJ-Song is Handsome_and_super_intellectual");
 	printf("Enter your expression: ");
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
