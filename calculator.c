9707a77a7afd40a3c0da09808e2eda2410ae997c
Oniqued <zktl3575@gmail.com>
2022-12-23 15:50:35 +0900

    201911506 김수현 changed calculator.c

--- calculator.c
+++ calculator.c
@@ -6,6 +6,7 @@
 int main(void) {
 	char input[BUFFERSIZE];
 	
+	printf("- 201911506 KIM SUHYUN's Assignment -\n")
 	printf("Enter your expression: ");
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
