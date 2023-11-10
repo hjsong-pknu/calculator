01de4c56dce5981ba13925ed24d6db2f65f5493c
jyyyjj023 <jyyyjj023@naver.com>
2023-11-10 17:50:36 +0900

    201912512 조진우

--- calculator.c
+++ calculator.c
@@ -5,7 +5,8 @@
 
 int main(void) {
 	char input[BUFFERSIZE];
-	char a[] = "text added";
+        int var = 36;
+
 	printf("Enter your expression: ");
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
