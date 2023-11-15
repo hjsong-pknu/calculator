f6a9146c9e64394d84faba3f0207a570318ecffd
jiinchoi <jiinchoi@jiinchoi>
2023-11-15 20:27:34 +0900

    202130467

--- calculator.c
+++ calculator.c
@@ -5,11 +5,12 @@
 
 int main(void) {
 	char input[BUFFERSIZE];
-	
+        
+
+	printf("hello calculator!");
 	printf(" >>> Text Based Calculator <<<\n");
 	printf("Enter your expression: ");
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
 	printf("Result is: %.2f\n", evaluate(input));
 	return EXIT_SUCCESS;
-}
