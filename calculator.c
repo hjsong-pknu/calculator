562dd11285634d5a893f32960df7da96606da064
전준휘 <132906110+gen0524@users.noreply.github.com>
2023-11-15 19:48:25 +0900

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -4,11 +4,33 @@
 #define BUFFERSIZE (256)
 
 int main(void) {
-	char input[BUFFERSIZE];
-	
-	printf("Enter your expression: ");
-	fflush(stdout);
-	fgets(input, BUFFERSIZE, stdin);
-	printf("Result is: %.2f\n", evaluate(input));
-	return EXIT_SUCCESS;
+    char input[BUFFERSIZE];
+    char studentID[BUFFERSIZE];
+    char name[BUFFERSIZE];
+    
+    // Get expression input
+    printf("Enter your expression: ");
+    fflush(stdout);
+    fgets(input, BUFFERSIZE, stdin);
+    
+    // Get student ID
+    printf("Enter your student ID: ");
+    fflush(stdout);
+    fgets(studentID, BUFFERSIZE, stdin);
+    
+    // Get name
+    printf("Enter your name: ");
+    fflush(stdout);
+    fgets(name, BUFFERSIZE, stdin);
+
+    // Output the result of the expression
+    printf("Result is: %.2f\n", evaluate(input));
+
+    // Print commit message (this would typically be used externally)
+    printf("\nCommit message:\n");
+    printf("Student ID: %s", studentID);
+    printf("Name: %s", name);
+    printf("Evaluated expression: %s", input);
+    
+    return EXIT_SUCCESS;
 }
